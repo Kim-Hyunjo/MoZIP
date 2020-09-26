7560f06121ba6e45d979284ddbd129e7d8aa2e11
parkwoongki <sbe03005@naver.com>
2020-09-26 16:17:10 +0900

    feat: Add Test Woongki

--- front/pages/test.tsx
+++ front/pages/test.tsx
@@ -3,18 +3,22 @@ import Link from "next/link";
 import Layout from "../components/Layout";
 import List from "../components/List";
 import { User } from "../interfaces";
+import { Test } from "../interfaces/test";
 import { sampleUserData } from "../utils/sample-data";
+import Axios from "axios";
 
 type Props = {
-  items: User[];
+  test1: Test;
 };
 
-const getTest = ({ items }: Props) => {
+const getTestProps = ({ test1 }: Props) => {
   return (
     <Layout title="Axios Test">
       <h1>Users List</h1>
       <p>박웅기 Test</p>
-      <p>{}</p>
+      <p>{test1.name}</p>
+      <p>{test1.project}</p>
+      <p>{test1.date}</p>
       <p>왕희도 Test</p>
       <p>{}</p>
       <p>한채은 Test</p>
@@ -23,9 +27,13 @@ const getTest = ({ items }: Props) => {
   );
 };
 
-export const dataFetchTest: GetStaticProps = async () => {
-  const items: User[] = sampleUserData;
-  return { props: { items } };
+export const getStaticProps: GetStaticProps = async () => {
+  const getTest = await Axios.get("https://skhu-pwk.firebaseio.com/test.json");
+  const test1: Test = getTest.data;
+
+  console.log(test1);
+
+  return { props: { test1 } };
 };
 
-export default getTest;
+export default getTestProps;
