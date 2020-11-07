4108099a3d0164271ba2b20b24997c5c72c4d4ec
stlts7 <xkq1998@gmail.com>
2020-11-07 14:55:14 +0900

    Add list style

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -20,38 +20,39 @@ function App() {
     <Router>
       <div className="App">
         <header>
-          <h1>여기는 헤더</h1>
           <nav>
-            <ul>
-              {/* <li><Link to="/">Index</Link></li>
-              <li><Link to="/home">Home</Link></li>
-              <li><Link to="/intro">Intro</Link></li> */}
-              {/* NavLink를 이용하면 다이나믹 스타일 적용가능 */}
-              <li><NavLink exact={true} activeStyle={{ fontSize: 24 }} to="/">Index</NavLink></li>
-              <li><NavLink activeStyle={{ fontSize: 24 }} to="/home">Home</NavLink></li>
-              <li><NavLink activeStyle={{ fontSize: 24 }} to="/intro">Intro</NavLink></li>
-            </ul>
+            <div id = "list">
+              <ul>
+                {/* <li><Link to="/">Index</Link></li>
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/intro">Intro</Link></li> */}
+                {/* NavLink를 이용하면 다이나믹 스타일 적용가능 */}
+                <li><NavLink exact to="/">My Page</NavLink></li>
+                <li><NavLink exact to="/">List</NavLink></li>
+                <li><NavLink exact to="/intro">Guide</NavLink></li>
+                <li><NavLink exact={true} to="/home">Home</NavLink></li>
+              </ul>
+            </div>
           </nav>
         </header>
         <hr />
         <h1>여기는 바디</h1>
         <Switch>
-          <Route exact={true} path="/" render={() => <h3>Index</h3>} />
+          <Route path="/" render={() => <h3>Index</h3>} />
           <Route exact={true} path="/home" component={Home} />
           <Route path="/intro" render={() => <h3>소개</h3>} />
           <Route path="/posts" component={TestList} />
           <Route path="/rt" component={RedirectTest} /> {/* 리다이렉트 방법*/}
           <Route exact path="/list/전체" component={ListPage1}/>
-  <Route exact path="/list/학술" component={ListPage2}/>
-  <Route exact path="/list/예술" component={ListPage3}/>
-  <Route exact path="/list/친목" component={ListPage4}/>
-  <Route exact path="/list/스포츠" component={ListPage5}/>
-  <Route exact path="/list/여행" component={ListPage6}/>
-  <Route exact path="/list/봉사" component={ListPage7}/>
-  <Route exact path="/list/학술/1" component={detailPage1}/>
-  <Route exact path="/자기소개함" component={SelfIntroduction} />
+          <Route exact path="/list/학술" component={ListPage2}/>
+          <Route exact path="/list/예술" component={ListPage3}/>
+          <Route exact path="/list/친목" component={ListPage4}/>
+          <Route exact path="/list/스포츠" component={ListPage5}/>
+          <Route exact path="/list/여행" component={ListPage6}/>
+          <Route exact path="/list/봉사" component={ListPage7}/>
+          <Route exact path="/list/학술/1" component={detailPage1}/>
+          <Route exact path="/자기소개함" component={SelfIntroduction} />
           <Route component={NotFound} />
-              
         </Switch>
         <hr />
         <footer>
