60308942f579c141bc400446bcae480c6261e878
stlts7 <xkq1998@gmail.com>
2020-11-07 16:50:17 +0900

    feat: add and modify list

--- front/src/pages/list/ListAll.tsx
+++ front/src/pages/list/ListAll.tsx
@@ -1,28 +1,31 @@
 import { BrowserRouter as Router, Route,Link } from 'react-router-dom';
 import React, {  useState } from "react";
 
-const Listpage1 = () => {
+const ListAll = () => {
     const [test,setSubject]  = useState([{name:'전체'},{name:'학술'},{name:'예술'},{name: '친목'},{name: '스포츠'}, {name:'여행'},{ name:'종교'},{name:'봉사'}])
 
 
-const 학술 = [{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}
+const 학술 = [{name: "학술", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}
 ,{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}];
-const 예술 = [{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}
+const 예술 = [{name: "예술", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}
 ,{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}]   
 
-const 친목 = [{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}
+const 친목 = [{name: "친목", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}
 ,{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}]   
 
-const 스포츠 = [{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}
+const 스포츠 = [{name: "스포츠", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}
 ,{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}]   
 
-const 여행 = [{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}
+const 여행 = [{name: "여행", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}
 ,{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}]   
 
-const 종교 = [{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}
+const 종교 = [{name: "종교", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}
 ,{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}]   
 
-const 봉사 = [{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}
+const 봉사 = [{name: "봉사", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}
+,{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}]   
+
+const 기타 = [{name: "기타", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}
 ,{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"},{name: "Tave", introduction:"국내 최초 4차 산업혁명동아리"}]   
 
 const handlerSubject =  (e:any) =>{
@@ -32,34 +35,34 @@ const handlerSubject =  (e:any) =>{
 
 return(
        <>
-            <div>
+         <div>
             <h2>전체 동아리 목록</h2>
             <p>MOZIP에 등록된 전체 동아리 목록을 확인해보세요.</p>
             <div>
             <label>
-            <select id="list-name" >
-            {test.map((item,index)=> { 
+                <select id="list-name" >
+                    {test.map((item,index)=> { 
+                        return(
+                            <option key={index} value={item.name}>
+                                {item.name}
+                            </option>
 
-                return(
-                <option key={index}
-                value={item.name} onChange={handlerSubject}>
-                    {item.name}
-                        </option>
-                    )
-                })}
-                        
-                        </select>
+                        )
+                    })}            
+                </select>
             </label>
-        </div>
+        </div> 
         <div>
             <ul>
-                <li><Link to="/list/전체">전체</Link></li>
-                <li><Link to="/list/학술">학술</Link></li>
-                <li><Link to="/list/예술">예술</Link></li>
-                <li><Link to="/list/친목">친목</Link></li>
-                <li><Link to="/list/스포츠">스포츠</Link></li>
-                <li><Link to="/list/여행">여행</Link></li>
-                <li><Link to="/list/봉사">봉사</Link></li>
+                <li><Link to="/list">전체</Link></li>
+                <li><Link to="/list/academy">학술</Link></li>
+                <li><Link to="/list/art">예술</Link></li>
+                <li><Link to="/list/networking">친목</Link></li>
+                <li><Link to="/list/sports">스포츠</Link></li>
+                <li><Link to="/list/travel">여행</Link></li>
+                <li><Link to="/list/religion">종교</Link></li>
+                <li><Link to="/list/volunteer">봉사</Link></li>
+                <li><Link to="/list/etc">기타</Link></li>
             </ul>
         </div>
     </div>
@@ -113,4 +116,4 @@ return(
     
 }
 
-export default Listpage1
+export default ListAll
---
+++ front/src/pages/signup.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const signup = () => {
+    return (
+        <h3>signup</h3>
+    );
+}
+
+export default signup;
