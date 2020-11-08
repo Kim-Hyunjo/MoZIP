df5f7726054b6508603f9891f51c26c6e14b02ae
parkwoongki <sbe03005@naver.com>
2020-11-08 18:25:49 +0900

    feat: Pull Project

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch, NavLink } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  NavLink,
+} from 'react-router-dom';
 import './App.css';
 import NotFound from './error/NotFound';
 import Home from './Test/home';
@@ -30,29 +36,70 @@ function App() {
       <div className="App">
         <header>
           <nav>
-            <div id = "list">
-              <ul className = "menu">
+            <div id="list">
+              <ul className="menu">
                 {/* NavLink를 이용하면 다이나믹 스타일 적용가능 */}
-                <li><NavLink exact to="/mypage">My Page</NavLink>
-                  <ul className ="sub">
-                    <li><NavLink exact to="/">지원 이력</NavLink></li>
-                    <li><NavLink exact to="/">자기소개서함</NavLink></li>
+                <li>
+                  <NavLink exact to="/mypage">
+                    My Page
+                  </NavLink>
+                  <ul className="sub">
+                    <li>
+                      <NavLink exact to="/">
+                        지원 이력
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink exact to="/">
+                        자기소개서함
+                      </NavLink>
+                    </li>
                   </ul>
                 </li>
-                <li><NavLink exact to="/list">List</NavLink>
-                  <ul className ="sub">
-                    <li><NavLink exact to="/list">전체 동아리</NavLink></li>
-                    <li><NavLink exact to="/">동아리 추가 개설</NavLink></li>
+                <li>
+                  <NavLink exact to="/list">
+                    List
+                  </NavLink>
+                  <ul className="sub">
+                    <li>
+                      <NavLink exact to="/list">
+                        전체 동아리
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink exact to="/">
+                        동아리 추가 개설
+                      </NavLink>
+                    </li>
                   </ul>
                 </li>
-                <li><NavLink exact to="/intro">Guide</NavLink>
-                  <ul className ="sub">
-                    <li><NavLink exact to="/">동아리 개설</NavLink></li>
-                    <li><NavLink exact to="/">동아리 운영</NavLink></li>
-                    <li><NavLink exact to="/">자기소개서함이란</NavLink></li>
+                <li>
+                  <NavLink exact to="/intro">
+                    Guide
+                  </NavLink>
+                  <ul className="sub">
+                    <li>
+                      <NavLink exact to="/">
+                        동아리 개설
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink exact to="/">
+                        동아리 운영
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink exact to="/">
+                        자기소개서함이란
+                      </NavLink>
+                    </li>
                   </ul>
                 </li>
-                <li><NavLink exact={true} to="/home">Home</NavLink></li>
+                <li>
+                  <NavLink exact={true} to="/home">
+                    Home
+                  </NavLink>
+                </li>
               </ul>
             </div>
           </nav>
@@ -64,23 +111,26 @@ function App() {
           <Route exact path="/" render={() => <h3>Index</h3>} />
           <Route exact path="/intro" render={() => <h3>소개</h3>} />
           <Route exact path="/posts" component={TestList} />
-          <Route exact path="/rt" component={RedirectTest} /> {/* 리다이렉트 방법*/}
-          <Route exact path="/list" component={ListAll}/>
-          <Route exact path="/list/academy" component={ListAcademy}/>
-          <Route exact path="/list/art" component={ListArt}/>
-          <Route exact path="/list/networking" component={ListNetworking}/>
-          <Route exact path="/list/sports" component={ListSports}/>
-          <Route exact path="/list/travel" component={ListTravel}/>
-          <Route exact path="/list/religion" component={ListReligion}/>
-          <Route exact path="/list/volunteer" component={ListVolunteer}/>
-          <Route exact path="/list/etc" component={ListETC}/>
-          <Route exact path="/list/학술/1" component={detailPage1}/>
-          
+          <Route exact path="/rt" component={RedirectTest} />{' '}
+          {/* 리다이렉트 방법*/}
+          <Route exact path="/list" component={ListAll} />
+          <Route exact path="/list/academy" component={ListAcademy} />
+          <Route exact path="/list/art" component={ListArt} />
+          <Route exact path="/list/networking" component={ListNetworking} />
+          <Route exact path="/list/sports" component={ListSports} />
+          <Route exact path="/list/travel" component={ListTravel} />
+          <Route exact path="/list/religion" component={ListReligion} />
+          <Route exact path="/list/volunteer" component={ListVolunteer} />
+          <Route exact path="/list/etc" component={ListETC} />
+          <Route exact path="/list/학술/1" component={detailPage1} />
           <Route exact path="/mypage" component={mypage} />
           <Route exact path="/mypage/edit" component={myPageEdit} />
           <Route exact path="/mypage/status" component={mypageStatus} />
-
-          <Route exact path="/mypage/introduction" component={SelfIntroduction} />
+          <Route
+            exact
+            path="/mypage/introduction"
+            component={SelfIntroduction}
+          />
           <Route component={NotFound} />
         </Switch>
         <hr />
@@ -88,16 +138,21 @@ function App() {
           <h1>여기는 푸터</h1>
           <nav>
             <ul>
-              <li><Link to="/">이용약관</Link></li>
-              <li><Link to="/home">개인정보 처리방침</Link></li>
-              <li><Link to="/intro">고객센터</Link></li>
+              <li>
+                <Link to="/">이용약관</Link>
+              </li>
+              <li>
+                <Link to="/home">개인정보 처리방침</Link>
+              </li>
+              <li>
+                <Link to="/intro">고객센터</Link>
+              </li>
             </ul>
             <div>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Optio amet deserunt repudiandae,
-              est reprehenderit unde culpa? Beatae,
-              cum iste vel, non eaque inventore alias quo maiores vero,
-              tenetur facilis quisquam?
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
+              amet deserunt repudiandae, est reprehenderit unde culpa? Beatae,
+              cum iste vel, non eaque inventore alias quo maiores vero, tenetur
+              facilis quisquam?
             </div>
           </nav>
         </footer>
@@ -105,6 +160,5 @@ function App() {
     </Router>
   );
 }
-
 
 export default App;
