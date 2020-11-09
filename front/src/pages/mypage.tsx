f1ded825b6e5a1440cf31b630eade3d3cb4b3cd8
stlts7 <xkq1998@gmail.com>
2020-11-09 20:04:21 +0900

    feat: modify mypage

--- front/src/pages/mypage.tsx
+++ front/src/pages/mypage.tsx
@@ -6,7 +6,7 @@ import "./button.css";
 const mypage = () => { 
   const personalInfo = {name: "최우영", sort:"직장인", school:"테이브대학교", major:"서핑학과", grade:"3", state:"재학", birth: "2020.11.07", phoneNum: "010-0000-0000", email: "tave@naver.com", address:"서울시 강남구"};
   const personalApplyCurrent = [{name: "~내가 지원한 동아리~", introduction:"~동아리의 상세설명~"},{name: "~내가 지원한 동아리2~", introduction:"~동아리의 상세설명2~"}]
-  const personalClublist = [{name: "~나의 동아리~", introduction:"~동아리의 상세설명~", position:"운영진"},{name: "~나의동아리2~", introduction:"~동아리의 상세설명2~", position:"회원"}]
+  const personalClublist = [{name: "TAVE", introduction:"동아리의 상세설명~~~~~", position:"운영진"},{name: "나의동아리2~~~~~", introduction:"~동아리의 상세설명2~", position:"회원"}]
   const personalOpenRequest = [{name: "~개설 요청 동아리~", introduction:"~동아리의 상세설명~", status:"진행중"},{name: "~개설 요청 동아리2~", introduction:"~동아리의 상세설명2~", status:"승인"}]
   const personalApplyHistory = [{name: "~내가 지원한 동아리~", introduction:"~동아리의 상세설명~", status:"합격"},{name: "~내가 지원한 동아리2~", introduction:"~동아리의 상세설명2~", status:"불합격"}]
   
@@ -35,7 +35,7 @@ const mypage = () => {
       <div id="applyCurrent">
         <div className ="middle_title">나의 지원 현황</div>
         <div className = "horizontal_scroll">
-          <ul className="my_first_namecard">
+          <ul className="small_namecard">
             <li>
               <div className = "club_name">{personalApplyCurrent[0].name}</div>
               <div className = "club_intro">{personalApplyCurrent[0].introduction}</div>
@@ -69,55 +69,62 @@ const mypage = () => {
           </ul>
         </div>
       </div>
-      <div id="applyHistory">
-      <div className ="middle_title">My 동아리</div>
+      <div id="myClub">
+        <div className ="middle_title">My 동아리</div>
         <div className="word_button">
           <Link to="./mypage/status">자세히 보기</Link>
         </div>
-        <ul id="small box">
-          <li>
-            {personalClublist[0].name} <br></br>
-            {personalClublist[0].position}<br></br>
-            {personalClublist[0].introduction}<br></br>
-          </li>
-          <li>
-            {personalClublist[1].name} <br></br>
-            {personalClublist[1].position}<br></br>
-            {personalClublist[1].introduction}<br></br>
-          </li>
-        </ul>
+        <div className="horizontal_scroll">
+          <ul className="small_namecard">
+            <li>
+              <div className = "club_position">{personalClublist[0].position}</div>
+              <div className = "club_name">{personalClublist[0].name}</div>
+              <div className = "club_intro">{personalClublist[0].introduction}</div>
+            </li>
+            <li>
+              <div className = "club_position">{personalClublist[1].position}</div>
+              <div className = "club_name">{personalClublist[1].name}</div>
+              <div className = "club_intro">{personalClublist[1].introduction}</div>
+            </li>
+          </ul>
+        </div>
       </div>
       <div id="openRequest">
-        동아리 개설 요청
-        <ul id="small box">
-          <li>
-            {personalOpenRequest[0].name} <br></br>
-            {personalOpenRequest[0].introduction}<br></br>
-            {personalOpenRequest[0].status}<br></br>
-          </li>
-          <li>
-            {personalOpenRequest[1].name} <br></br>
-            {personalOpenRequest[1].introduction}<br></br>
-            {personalOpenRequest[1].status}<br></br>
-          </li>
-        </ul>
-        <button>개설 요청하기</button>
+        <div className ="middle_title">개설 요청한 동아리</div>
+        <div className="word_button">
+          <Link to="./">동아리 개설하기</Link>
+        </div>
+        <div className="horizontal_scroll">
+          <ul className="small_namecard">
+            <li>
+              <div className = "club_position">{personalOpenRequest[0].status}</div>
+              <div className = "club_name">{personalOpenRequest[0].name}</div>
+              <div className = "club_intro">{personalOpenRequest[0].introduction}</div>
+            </li>
+            <li>
+              <div className = "club_position">{personalOpenRequest[1].status}</div>
+              <div className = "club_name">{personalOpenRequest[1].name}</div>
+              <div className = "club_intro">{personalOpenRequest[1].introduction}</div>
+            </li>
+          </ul>
+        </div>  
       </div>
       <div id="applyHistory">
-        나의 지원 이력
-        <ul id="small box">
-          <li>
-            {personalApplyHistory[0].name} <br></br>
-            {personalApplyHistory[0].status} <br></br>
-            {personalApplyHistory[0].introduction}<br></br>
-          </li>
-          <li>
-            {personalApplyHistory[1].name} <br></br>
-            {personalApplyHistory[1].status} <br></br>
-            {personalApplyHistory[1].introduction}<br></br>
-            <button>지원서 수정하기</button>
-          </li>
-        </ul>
+        <div className ="middle_title">나의 지원 이력</div>
+        <div className="horizontal_scroll">  
+          <ul className="small_namecard">
+            <li>
+              <div className = "club_position">{personalApplyHistory[0].status}</div>
+              <div className = "club_name">{personalApplyHistory[0].name}</div>
+              <div className = "club_intro">{personalApplyHistory[0].introduction}</div>
+            </li>
+            <li>
+              <div className = "club_position">{personalApplyHistory[1].status}</div>
+              <div className = "club_name">{personalApplyHistory[1].name}</div>
+              <div className = "club_intro">{personalApplyHistory[1].introduction}</div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   )
