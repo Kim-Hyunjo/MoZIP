8df2e0961683db1c8ae93c14b37332e6446037fb
parkwoongki <sbe03005@naver.com>
2020-11-14 15:08:54 +0900

    feat: Add recruit/selection Link

--- front/src/pages/recruit/Selection.tsx
+++ front/src/pages/recruit/Selection.tsx
@@ -7,7 +7,11 @@ const Selection = () => {
       <h2>모집 전형 선택하기</h2>
       <div>부원 모집 전형을 선택해주세요.</div>
       <div>
-        <button>서류 + 면접 전형</button>
+        {/* <button>서류 + 면접 전형</button> */}
+
+        <Link to="/recruit/process/basicinfo">
+          <button>서류 + 면접 전형</button>
+        </Link>
         <div>
           1차로 서류(자기소개서)를 제출받고, 2차로 면접을 진행하는 전형입니다.
         </div>
