4be1983db4113c35ea8abc2b4147c2f7811b4e41
stlts7 <xkq1998@gmail.com>
2020-11-14 15:18:57 +0900

    feat: edit

--- front/src/pages/circle/CircleMembers.tsx
+++ front/src/pages/circle/CircleMembers.tsx
@@ -3,69 +3,60 @@ import React, { useState } from 'react';
 
 const CircleMembers = () => {
     return (
-        <div>
+        <div className="circleMembers">
             <div>
                 <h2>회원 명단</h2>
-                <span>내 동아리의 부원 명단을 정리 및 확인해보세요.</span>
+                <h4>내 동아리의 부원 명단을 정리 및 확인해보세요.</h4>
              </div>
-             <div>
-                <h3>총 160명</h3>
-                <h4>운영진 및 동아리 부원</h4>
+             <div className="membersSort">
+                <div className="memberNum">총 160명</div>
+                <button id="button3">운영진 수정</button>
+                <div className="members_title">운영진 및 동아리 부원</div>
                
-                <ul>
+                <ul className="membersKind">
                     <li>회장</li>
                     <li>기술처</li>
                     <li>회원</li>
                 </ul>
-                <button>운영진 수정</button>
             </div>
             <div>
-                <h2>회장</h2>
-                <div><h3>최우영</h3><strong>회장</strong><span>프로필 사진</span></div>
-            </div>
-
-            <div>
-                <h2>기술처</h2>
-                <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
+                <div className="roleName">회장</div>
+                <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">회장</div><span>프로필 사진</span></div>
             </div>
 
             <div>
-                <h2>기술처</h2>
-                <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
+                <div className="roleName">기술처</div>
+                <div className="user_namecard_list">
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">기술</div><span>프로필 사진</span></div>
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">기술</div><span>프로필 사진</span></div>
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">기술</div><span>프로필 사진</span></div>
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">기술</div><span>프로필 사진</span></div>
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">기술</div><span>프로필 사진</span></div>
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">기술</div><span>프로필 사진</span></div>
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">기술</div><span>프로필 사진</span></div>
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">기술</div><span>프로필 사진</span></div>
+                </div>
             </div>
+
             <div>
-                <h2>기술처</h2>
-                <ul>
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-                    
-
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-                    <li><h3>최우영</h3><strong>기술</strong><span>프로필 사진</span></li>
-                    
-                </ul>
-            
+                <div className="roleName">회원</div>
+                <div className="user_namecard_list">
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">회원</div><span>프로필 사진</span></div>
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">회원</div><span>프로필 사진</span></div>
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">회원</div><span>프로필 사진</span></div>
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">회원</div><span>프로필 사진</span></div>
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">회원</div><span>프로필 사진</span></div>
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">회원</div><span>프로필 사진</span></div>
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">회원</div><span>프로필 사진</span></div>
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">회원</div><span>프로필 사진</span></div>
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">회원</div><span>프로필 사진</span></div>
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">회원</div><span>프로필 사진</span></div>
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">회원</div><span>프로필 사진</span></div>
+                    <div className="user_namecard"><div className="user_name">최우영</div><div className="user_role">회원</div><span>프로필 사진</span></div>
+                </div>
             </div>
 
-            </div>
+        </div>
     )
 }
 
