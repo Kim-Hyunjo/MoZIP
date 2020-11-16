6034a6cfebc79287c8f65ba57108462e885750e2
parkwoongki <sbe03005@naver.com>
2020-11-17 01:11:15 +0900

    feat: Add 로그인 다이얼로그 띄움

--- front/src/pages/login/KakaoSignUp2.tsx
+++ front/src/pages/login/KakaoSignUp2.tsx
@@ -1,12 +1,69 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import styled from 'styled-components';
 // import { StyledText } from '../style';
 import KaKaoLogin from 'react-kakao-login';
 import axios from 'axios';
 import Cookies from 'universal-cookie';
 
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import PersonIcon from '@material-ui/icons/Person';
+import Typography from '@material-ui/core/Typography';
+import { blue } from '@material-ui/core/colors';
+import Button from '@material-ui/core/Button';
+
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+    small: {
+      width: theme.spacing(3),
+      height: theme.spacing(3),
+    },
+    large: {
+      width: theme.spacing(7),
+      height: theme.spacing(7),
+    },
+  }),
+);
+
 const KakaoSignUp = () => {
+  const classes = useStyles();
+
+  const [img, setImg] = useState<string>('');
   const cookies = new Cookies();
+
+  useEffect(() => {
+    console.log('컴포넌트가 화면에 나타남');
+    return () => {
+      console.log('컴포넌트가 화면에서 사라짐');
+    };
+  }, [img]);
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const responseKaKao = (res: any) => {
     const access_token = JSON.stringify(res.response.access_token);
@@ -21,8 +78,10 @@ const KakaoSignUp = () => {
     cookies.set('image', image, { path: '/' });
     cookies.set('email', email, { path: '/' });
     cookies.set('access_token', access_token, { path: '/' });
-
-    alert(JSON.stringify(res));
+    setImg(image);
+    console.log(image);
+    handleClickOpen();
+    // alert(JSON.stringify(res));
     // console.log(JSON.stringify(res));
   };
 
@@ -42,6 +101,34 @@ const KakaoSignUp = () => {
           onFail={responseFail}
         />
       </div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{cookies.get('name')}</DialogTitle>
+        <DialogContent>
+          <div className={classes.root}>
+            <Avatar
+              alt="Remy Sharp"
+              src={cookies.get('image')}
+              className={classes.large}
+            />
+          </div>
+          <DialogContentText id="alert-dialog-description">
+            로그인하시겠습니까?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            아뉴
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            네
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
