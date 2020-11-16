0f5121ebc11e242a286ebb5a9126d4ade8bbdcf3
heedo-wang <dhkdgmleh@gmail.com>
2020-11-16 23:23:21 +0900

    feat: add pageedit axios

--- front/src/pages/mypage/myPageEdit.tsx
+++ front/src/pages/mypage/myPageEdit.tsx
@@ -9,20 +9,48 @@ import { DialogContentText } from '@material-ui/core';
 import { DialogTitle } from '@material-ui/core';
 
 import './namecard.css';
+import Axios from 'axios';
 
-const myPageEdit = () => {
-  const personalInfo = {
-    name: '최우영',
-    sort: '직장인',
-    school: '테이브대학교',
-    major: '서핑학과',
-    grade: '3',
-    state: '재학',
-    birth: '2020.11.07',
-    phoneNum: '010-0000-0000',
-    email: 'tave@naver.com',
-    address: '서울시 강남구',
-  };
+const MyPageEdit:any = () => {
+
+  const [name, setname] = useState('')
+  const [sort, setsort] = useState('')
+  const [school, setschool] = useState('')
+  const [major, setmajor] = useState('')
+  const [grade, setgrade] = useState('')
+  const [birth, setbirth] = useState('')
+  const [phoneNum, setphoneNum] = useState('')
+  const [email, setemail] = useState('')
+  const [address, setaddress] = useState('')
+ 
+  const handleChangeName:any =(e:Event) =>{
+    setname((e.currentTarget as any).value)
+  }
+  const handleChangeSort:any =(e:Event) =>{
+    setsort((e.currentTarget as any).value)
+  }
+  const handleChangeSchool:any =(e:Event) =>{
+    setschool((e.currentTarget as any).value)
+  }
+  const handleChangeMajor:any =(e:Event) =>{
+    setmajor((e.currentTarget as any).value)
+  }
+  const handleChangeGrade:any =(e:Event) =>{
+    setgrade((e.currentTarget as any).value)
+  }
+  const handleChangeBirth:any =(e:Event) =>{
+    setbirth((e.currentTarget as any).value)
+  }
+  const handleChangePhoneNum:any =(e:Event) =>{
+    setphoneNum((e.currentTarget as any).value)
+  }
+  const handleChangeEmail:any =(e:Event) =>{
+    setemail((e.currentTarget as any).value)
+  }
+  const handleChangeAddress:any =(e:Event) =>{
+    setaddress((e.currentTarget as any).value)
+  }
+
   const personalApplyHistory = [
     { name: '~내가 지원한 동아리~', introduction: '~동아리의 상세설명~' },
     { name: '~내가 지원한 동아리2~', introduction: '~동아리의 상세설명2~' },
@@ -35,10 +63,15 @@ const myPageEdit = () => {
     setOpen(true);
   };
 
-  const handleClose = () => {
+  const handleClose1 = () => {
     setOpen(false);
-  };
-
+    Axios.post("https://skhu-pwk.firebaseio.com/todo1.json",{
+      name: name
+  });
+  }
+  const handleClose2 = () =>{
+    setOpen(false);
+  }
   return (
     <div>
       <h2>개인정보 수정</h2>
@@ -52,96 +85,113 @@ const myPageEdit = () => {
                 <label htmlFor="firstName">이름:</label>
                 <input
                   type="text"
-                  className="mypageEdit"
-                  value={personalInfo.name}
+                  className="MyPageEdit"
                   placeholder="이름"
                   name="name"
+                  onChange={handleChangeName}
+                  value={name}
                 />
               </div>
               <div className="분류">
                 <label htmlFor="분류">분류:</label>
                 <input
                   type="text"
-                  className="mypageEdit"
+                  className="MyPageEdit"
                   placeholder="분류"
                   name="name"
+                  onChange={handleChangeSort}
+                  value={sort}
                 />
               </div>
               <div className="학교">
                 <label htmlFor="학교">학교:</label>
                 <input
                   type="text"
-                  className="mypageEdit"
+                  className="MyPageEdit"
                   placeholder="학교"
                   name="name"
+                  onChange={handleChangeSchool}
+                  value={school}
                 />
               </div>
               <div className="전공">
                 <label htmlFor="전공">전공:</label>
                 <input
                   type="text"
-                  className="mypageEdit"
+                  className="MyPageEdit"
                   placeholder="전공"
                   name="name"
+                  onChange={handleChangeMajor}
+                  value={major}
                 />
               </div>
               <div className="학년">
                 <label htmlFor="학년">학년:</label>
                 <input
                   type="text"
-                  className="mypageEdit"
+                  className="MyPageEdit"
                   placeholder="학년"
                   name="name"
+                  onChange={handleChangeGrade}
+                  value={grade}
                 />
               </div>
               <div className="생년월일">
                 <label htmlFor="생년월일">생년월일:</label>
                 <input
                   type="text"
-                  className="mypageEdit"
+                  className="MyPageEdit"
                   placeholder="생년월일"
                   name="name"
+                  onChange={handleChangeBirth}
+                  value={birth}
                 />
               </div>
               <div className="전화번호">
                 <label htmlFor="firstName">전화번호:</label>
                 <input
                   type="text"
-                  className="mypageEdit"
+                  className="MyPageEdit"
                   placeholder="전화번호"
                   name="name"
+                  onChange={handleChangePhoneNum}
+                  value={phoneNum}
                 />
               </div>
               <div className="이메일">
                 <label htmlFor="이메일">이메일:</label>
                 <input
                   type="text"
-                  className="mypageEdit"
+                  className="MyPageEdit"
                   placeholder="이메일"
                   name="name"
+                  onChange={handleChangeEmail}
+                  value={email}
                 />
               </div>
               <div className="거주지">
                 <label htmlFor="거주지">거주 지역:</label>
                 <input
                   type="text"
-                  className="mypageEdit"
+                  className="MyPageEdit"
                   placeholder="거주지"
                   name="name"
+                  onChange={handleChangeAddress}
+                  value={address}
                 />
               </div>
             </form>
           </div>
         </div>
-        <div className="mypageEditButton">
+        <div className="MyPageEditButton">
           {/* <Link to="/mypage"> */}
-          <button id="button4" onClick={handleClickOpen}>완료</button>
+          <button id="button4" onClick={handleClickOpen }>완료</button>
           {/* </Link> */}
         </div>
       </div>
       <Dialog
         open={open}
-        onClose={handleClose}
+        onClose={handleClose2}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
@@ -157,14 +207,14 @@ const myPageEdit = () => {
 
         <DialogActions>
           <Link to="/mypage">
-            <Button onClick={handleClose} color="primary" autoFocus>
+            <Button type="submit"  onClick={handleClose1} color="primary" autoFocus>
               예
             </Button>
           </Link>
           {/* <Button onClick={handleClose} color="primary" autoFocus>
             예
           </Button> */}
-          <Button onClick={handleClose} color="primary">
+          <Button onClick={handleClose2} color="primary">
             아니오
           </Button>
         </DialogActions>
@@ -173,4 +223,4 @@ const myPageEdit = () => {
   );
 };
 
-export default myPageEdit;
+export default MyPageEdit;
