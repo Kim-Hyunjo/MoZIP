983f10ffb8511df6e78f12d35c908ca566b2e77d
stlts7 <xkq1998@gmail.com>
2020-11-18 20:10:01 +0900

    feat: edit

---
+++ front/src/pages/about/guideSelfintroduction.tsx
@@ -0,0 +1,17 @@
+import React,{useState} from 'react'
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Switch,
+    NavLink,
+  } from 'react-router-dom';
+const guideSelfintroduction = () => {
+  return(
+    <div>
+      guideSelfintroduction
+    </div>
+  )
+}
+
+export default guideSelfintroduction;
