fd113b82e2c93f447a82534ff49554bc95c75832
parkwoongki <sbe03005@naver.com>
2020-11-20 23:02:04 +0900

    feat: recruit/resume Resume 페이지 구현 완료

---
+++ front/src/pages/recruit/ResumeCandidate.tsx
@@ -0,0 +1,44 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+interface Candidate {
+  id: number;
+  name: string;
+  group: string;
+  time: string;
+  score: number;
+  status: string;
+  isChecked: boolean;
+}
+
+interface Props {
+  candidate: Candidate;
+  onChangeCheck(id: number, isChecked: boolean): void;
+}
+
+const ResumeCandidate = ({ candidate, onChangeCheck }: Props) => {
+  const handleChangeCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChangeCheck(candidate.id, e.target.checked);
+  };
+
+  return (
+    <Fragment>
+      <label htmlFor=""></label>
+      <input type="checkbox" onChange={handleChangeCheck} />
+      <span className="personalName">{candidate.name} </span>
+      <span className="personalTime">
+        {candidate.group}
+        {candidate.time}
+      </span>
+      <div className="rightSide">
+        <span className="personalScore"> 점수 :{candidate.score}점</span>
+        <strong id="skip">{candidate.status}</strong>
+        <Link to="/recruit/resume/:member_id">
+          <button id="button1">상세보기</button>
+        </Link>
+      </div>
+    </Fragment>
+  );
+};
+
+export default ResumeCandidate;
