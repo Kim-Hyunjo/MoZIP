70ddbf869be2fe62bbddfdf11a76723bb37e2ca0
heedo-wang <dhkdgmleh@gmail.com>
2020-11-22 01:59:07 +0900

    feat: modify list

--- front/src/pages/list/List.tsx
+++ front/src/pages/list/List.tsx
@@ -4,9 +4,7 @@ import {
   Link,
   RouteComponentProps,
 } from 'react-router-dom';
-import React, { useState ,useEffect} from 'react';
-import ListAcademy from './ListAcademy';
-import ListSubject from './ListSubject';
+import React, { useState, useEffect } from 'react';
 import './tab.css';
 import icon_all from '../images2/icon_all@2x.png';
 import icon_academy from '../images2/icon_academy@2x.png';
@@ -17,11 +15,28 @@ import icon_trip from '../images2/icon_trip@2x.png';
 import icon_religion from '../images2/icon_religion@2x.png';
 import icon_volunteer from '../images2/icon_volunteer@2x.png';
 import icon_etc from '../images2/icon_etc@2x.png';
-import axios from'axios'
+import axios from 'axios'
+import { DevelopUrl } from '../../http/HttpUrl';
 
+interface Subject{
+id:Number,
+name:String,
+introduction:String
+}
 const List = (props: RouteComponentProps<{}>) => {
-  
+
   const [subject, setSubject] = useState<string>('all');
+  
+  const getSubject = async() =>{
+    await axios.get('http://3.35.234.131:8000/circle/open/reject/users/').then((r)=>{
+      let res = r.data;
+      console.log(res)
+      setcontent(res)
+      console.log(content)
+    })
+    console.log(subject)
+  }
+
   const test = [
     { name: 'all', img: icon_all },
     { name: 'academy', img: icon_academy },
@@ -33,104 +48,148 @@ const List = (props: RouteComponentProps<{}>) => {
     { name: 'volunteer', img: icon_volunteer },
     { name: 'etc', img: icon_etc },
   ];
-  const 학술 = [
-    { id: '1', name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { id: '2', name: '학술', introduction: '국내 최초 4차 산업혁명동아리' },
-    { id: '3', name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { id: '4', name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { id: '5', name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { id: '6', name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { id: '7', name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { id: '8', name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { id: '9', name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+  const all = [
+    { id: 1, name: 'all', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 2, name: 'all', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 3, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 4, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 5, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 6, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 7, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 8, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 9 , name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
   ];
-  const 예술 = [
-    { name: '예술', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-  ];
-
-  const 친목 = [
-    { name: '친목', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+  const academy = [
+    { id: 1, name: '학술', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 2, name: '학술', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 3, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 4, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 5, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 6, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 7, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 8, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 9 , name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
   ];
 
-  const 스포츠 = [
-    { name: '스포츠', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+  const art = [
+    { id: 1,name: '예술', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 2, name: '예술', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 3, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 4, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 5, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 6, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 7, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 8, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 9 , name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
   ];
 
-  const 여행 = [
-    { name: '여행', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+  const networking = [
+    { id: 1,name: '친목', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 2, name: '친목', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 3, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 4, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 5, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 6, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 7, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 8, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 9 , name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
   ];
 
-  const 종교 = [
-    { name: '종교', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+  const sports = [
+    { id: 1,name: '스포츠', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 2, name: '스포츠', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 3, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 4, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 5, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 6, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 7, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 8, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 9 , name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
   ];
 
-  const 봉사 = [
-    { name: '봉사', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+  const trip = [
+    {id: 1,name: '여행', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 2, name: '여행', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 3, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 4, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 5, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 6, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 7, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 8, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 9 , name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
   ];
 
-  const 기타 = [
-    { name: '기타', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
-    { name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+  const religion = [
+    {id: 1, name: '종교', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 2, name: '종교', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 3, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 4, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 5, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 6, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 7, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 8, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 9 , name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
   ];
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+  const volunteer = [
+    {id: 1, name: '봉사', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 2, name: '봉사', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 3, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 4, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 5, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 6, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 7, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 8, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 9 , name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+  ];
+
+  const etc = [
+    { id: 1, name: '기타', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 2, name: '기타', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 3, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 4, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 5, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 6, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 7, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 8, name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+    { id: 9 , name: 'Tave', introduction: '국내 최초 4차 산업혁명동아리' },
+  ]
+const [content, setcontent] = useState(all)
+
+  useEffect(():any => {
+    if(subject ==="all"){
+      setcontent(all)
+      getSubject()
+    }else
+    if (subject === "academy") {
+      setcontent(academy)
+    }
+    else
+    if (subject === "art") {
+      setcontent(art)
+    }
+    else
+    if (subject === "networking") {
+      setcontent(networking)
+    }
+    else
+    if (subject === "sports") {
+      setcontent(sports)
+    }else
+    if (subject === "trip") {
+      setcontent(trip)
+    }
+    else
+    if (subject === "religion") {
+      setcontent(religion)
+    }else
+    if (subject === "volunteer") {
+      setcontent(volunteer)
+    }else
+    if (subject === "etc") {
+      setcontent(etc)
+    }
+  }, [subject]);
 
   return (
     <div className="list">
@@ -159,22 +218,22 @@ const List = (props: RouteComponentProps<{}>) => {
         <div>
           <ul id="horizontal_list">
             {test.map((item) => {
-              if (item.name === subject){
-              return (
-                <li className="chosen" 
-                  onClick={() => {
-                    setSubject(item.name);
-                    console.log(item.name);
-                  }}
-                >
-                  <div className="border top_border border_b"></div>
-                  <img src={item.img} alt="tab_icon" />
-                  {item.name}
-                  <div className="border bottom_border border_b"></div>
-                </li>
-              );}else {
+              if (item.name === subject) {
                 return (
-                  <li 
+                  <li className="chosen"
+                    onClick={() => {
+                      setSubject(item.name);
+                    }}
+                  >
+                    <div className="border top_border border_b"></div>
+                    <img src={item.img} alt="tab_icon" />
+                    {item.name}
+                    <div className="border bottom_border border_b"></div>
+                  </li>
+                )
+              } else {
+                return (
+                  <li
                     className="notChosen"
                     onClick={() => {
                       setSubject(item.name);
@@ -196,7 +255,7 @@ const List = (props: RouteComponentProps<{}>) => {
         {/* <h1>{props.match.params.subject}</h1> */}
         <div className="club_list">
           <ul id="horizontal_club_list">
-            {학술.map((item, index) => {
+            {content.map((item, index) => {
               return (
                 <li>
                   <Link to={`${props.match.url}/${subject}/${item.id}`}>
