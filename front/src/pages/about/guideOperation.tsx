53e79bb0caaec35269b3e344f2efe72cc615246d
jadely-lab <jadely0819@gmail.com>
2020-11-22 02:43:55 +0900

    feat: CSS changes including intro

--- front/src/pages/about/guideOperation.tsx
+++ front/src/pages/about/guideOperation.tsx
@@ -6,10 +6,16 @@ import {
     Switch,
     NavLink,
   } from 'react-router-dom';
+// import './trying_animation.css';
+// import banner1_words from '../images2/banner1_words@2x.png';
+// import imgA from '../images2/card_owner.png';
+// import imgB from '../images2/card_user.png';
+// import imgC from '../images2/whatis@2x.png';
+// import scroller from './animation.js'; 
 const guideOperation = () => {
   return(
     <div>
-      guideRegistration
+      
     </div>
   )
 }
