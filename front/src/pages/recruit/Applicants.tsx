f4a039313f7b9b95c4cb27444c5f452f12659f89
stlts7 <xkq1998@gmail.com>
2020-11-28 09:48:55 +0900

    feat : edit  resume

--- front/src/pages/recruit/Applicants.tsx
+++ front/src/pages/recruit/Applicants.tsx
@@ -1,33 +1,79 @@
-import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
 const Applicants = () => {
     return (
         <div className="applicants">
             <h2> 지원자 현황</h2>
             <div>
-                <div className="memberNum">총 160명</div>
+                <div className="memberNum">총 90명</div>
                
-                <button id = "button4">통계보기</button>
                 <ul className="user_namecard_list">
-                    <li className="user_namecard"><div className="user_name">최우영</div><span>프로필 사진</span></li>
-                    <li className="user_namecard"><div className="user_name">최우영</div><span>프로필 사진</span></li>
-                    <li className="user_namecard"><div className="user_name">최우영</div><span>프로필 사진</span></li>
-                    <li className="user_namecard"><div className="user_name">최우영</div><span>프로필 사진</span></li>
-                    <li className="user_namecard"><div className="user_name">최우영</div><span>프로필 사진</span></li>
+                    <li className="user_namecard"><div className="user_name">최우영</div>
+                    <div className="clubImage">
+                        <img src="http://placehold.it/120x120" />
+                    </div></li>
+                    <li className="user_namecard"><div className="user_name">박웅기</div>
+                    <div className="clubImage">
+                        <img src="http://placehold.it/120x120" />
+                    </div></li>
+                    <li className="user_namecard"><div className="user_name">왕희도</div>
+                    <div className="clubImage">
+                        <img src="http://placehold.it/120x120" />
+                    </div></li>
+                    <li className="user_namecard"><div className="user_name">햔채은</div>
+                    <div className="clubImage">
+                        <img src="http://placehold.it/120x120" />
+                    </div></li>
+                    <li className="user_namecard"><div className="user_name">한예송</div>
+                    <div className="clubImage">
+                        <img src="http://placehold.it/120x120" />
+                    </div></li>
 
-                    <li className="user_namecard"><div className="user_name">최우영</div><span>프로필 사진</span></li>
-                    <li className="user_namecard"><div className="user_name">최우영</div><span>프로필 사진</span></li>
-                    <li className="user_namecard"><div className="user_name">최우영</div><span>프로필 사진</span></li>
-                    <li className="user_namecard"><div className="user_name">최우영</div><span>프로필 사진</span></li>
-                    <li className="user_namecard"><div className="user_name">최우영</div><span>프로필 사진</span></li>
+                    <li className="user_namecard"><div className="user_name">최우영</div>
+                    <div className="clubImage">
+                        <img src="http://placehold.it/120x120" />
+                    </div></li>
+                    <li className="user_namecard"><div className="user_name">박웅기</div>
+                    <div className="clubImage">
+                        <img src="http://placehold.it/120x120" />
+                    </div></li>
+                    <li className="user_namecard"><div className="user_name">왕희도</div>
+                    <div className="clubImage">
+                        <img src="http://placehold.it/120x120" />
+                    </div></li>
+                    <li className="user_namecard"><div className="user_name">햔채은</div>
+                    <div className="clubImage">
+                        <img src="http://placehold.it/120x120" />
+                    </div></li>
+                    <li className="user_namecard"><div className="user_name">한예송</div>
+                    <div className="clubImage">
+                        <img src="http://placehold.it/120x120" />
+                    </div></li>
 
-                    <li className="user_namecard"><div className="user_name">최우영</div><span>프로필 사진</span></li>
-                    <li className="user_namecard"><div className="user_name">최우영</div><span>프로필 사진</span></li>
-                    <li className="user_namecard"><div className="user_name">최우영</div><span>프로필 사진</span></li>
-                    <li className="user_namecard"><div className="user_name">최우영</div><span>프로필 사진</span></li>
-                    <li className="user_namecard"><div className="user_name">최우영</div><span>프로필 사진</span></li>
+                    <li className="user_namecard"><div className="user_name">최우영</div>
+                    <div className="clubImage">
+                        <img src="http://placehold.it/120x120" />
+                    </div></li>
+                    <li className="user_namecard"><div className="user_name">박웅기</div>
+                    <div className="clubImage">
+                        <img src="http://placehold.it/120x120" />
+                    </div></li>
+                    <li className="user_namecard"><div className="user_name">왕희도</div>
+                    <div className="clubImage">
+                        <img src="http://placehold.it/120x120" />
+                    </div></li>
+                    <li className="user_namecard"><div className="user_name">햔채은</div>
+                    <div className="clubImage">
+                        <img src="http://placehold.it/120x120" />
+                    </div></li>
+                    <li className="user_namecard"><div className="user_name">한예송</div>
+                    <div className="clubImage">
+                        <img src="http://placehold.it/120x120" />
+                    </div></li>
                 </ul>
             </div>
+            <Link to = "/mypage/status"><button id="button4">확인</button></Link>
         </div>
     )
 }
